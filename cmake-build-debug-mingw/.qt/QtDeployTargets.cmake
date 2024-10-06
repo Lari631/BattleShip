7f9f49af891dacccd7c286c9f62f0e697ef3b080
unknown <munteanlarisamaria@gmail.com>
2024-10-06 22:09:36 +0300

    initial commit

---
+++ cmake-build-debug-mingw/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Battleship_FILE C:/Users/A/CLionProjects/Battleship/cmake-build-debug-mingw/Battleship.exe)
+set(__QT_DEPLOY_TARGET_Battleship_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
